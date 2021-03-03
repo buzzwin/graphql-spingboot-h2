7d25f5a8c916f31bb802fd670827bc7ad898e31d
gunjanv <61991609+gunschu@users.noreply.github.com>
2021-03-03 13:09:36 -0600

    first commit

---
+++ src/main/java/com/ohair/stephen/graphql/resolvers/PersonQuery.java
@@ -0,0 +1,26 @@
+package com.ohair.stephen.graphql.resolvers;
+
+import org.springframework.stereotype.Component;
+
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import com.ohair.stephen.graphql.exceptions.PersonNotFoundException;
+import com.ohair.stephen.graphql.model.Person;
+import com.ohair.stephen.graphql.repositories.PersonRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class PersonQuery implements GraphQLQueryResolver {
+
+    private final PersonRepository repo;
+
+    public Person person(Long id) {
+        return repo.findById(id).orElseThrow(() -> new PersonNotFoundException("person not found", id));
+    }
+
+    public Iterable<Person> allPeople() {
+        return repo.findAll();
+    }
+
+}
